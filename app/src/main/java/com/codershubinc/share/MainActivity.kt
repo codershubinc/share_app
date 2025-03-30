eea6b56ba637604134f425f8e8229ef1066017aa
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-03-31 03:19:35 +0530

    Update changes in android greeting

--- app/src/main/java/com/codershubinc/share/MainActivity.kt
+++ app/src/main/java/com/codershubinc/share/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.codershubinc.share.ui.theme.ShareTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,6 +27,8 @@ class MainActivity : ComponentActivity() {
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
+                            .border(50.dp, Color.Blue )
+                            .rotate(80.toFloat())
                     )
                 }
             }
@@ -33,7 +39,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello na  $name !",
         modifier = modifier
     )
 }
